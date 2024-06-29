c6f91e5949d4a21a8f3541bd752dc11218f163b6
Jussara Granja <jussaragranja19@gmail.com>
2024-06-29 13:42:13 -0300

    add exception and get user by id

--- src/main/java/com/hackaton/marjopay/controller/UserController.java
+++ src/main/java/com/hackaton/marjopay/controller/UserController.java
@@ -1,12 +1,12 @@
 package com.hackaton.marjopay.controller;
 
+import com.hackaton.marjopay.exception.ResourceNotFoundException;
 import com.hackaton.marjopay.model.User;
 import com.hackaton.marjopay.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static com.hackaton.marjopay.util.Constant.MESSAGE_USER_NOT_FOUND;
 
 import java.util.List;
 
@@ -18,10 +18,15 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/list-user")
+    @GetMapping("/user")
     public List<User> listUsers(){
         return userRepository.findAll();
     }
 
+    @GetMapping("/user/{id}")
+    public User getUserById(@PathVariable(value = "id") long id){
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(MESSAGE_USER_NOT_FOUND));
+    }
 
 }
--- src/main/java/com/hackaton/marjopay/util/Constant.java
+++ src/main/java/com/hackaton/marjopay/util/Constant.java
@@ -3,5 +3,6 @@ package com.hackaton.marjopay.util;
 public class Constant {
 
     public static final String MESSAGE_MAX_MIN_SIZE = "The field must contain between {min} and {max} characters.";
+    public static final String MESSAGE_USER_NOT_FOUND = "User not found!";
 
 }
