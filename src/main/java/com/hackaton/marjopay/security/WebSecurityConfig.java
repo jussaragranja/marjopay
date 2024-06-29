a56e40fcd1817a79ea57fb51a48429bad0fe665c
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-06-29 12:57:17 -0300

    Class webSecurityConfig configuration

---
+++ src/main/java/com/hackaton/marjopay/security/WebSecurityConfig.java
@@ -0,0 +1,37 @@
+package com.hackaton.marjopay.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+	
+    private static final String[] AUTH = {  "/v3/api-docs/**","/swagger-ui/**", "/swagger-ui.html","/swagger-resources/**", "/webjars/**"};
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(
+                		AUTH
+                ).permitAll()
+                .requestMatchers(HttpMethod.GET, "/**").permitAll()
+                .anyRequest().authenticated()
+            );
+//            .addFilterBefore(customBasicAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        
+        return http.build();
+    }
+}
