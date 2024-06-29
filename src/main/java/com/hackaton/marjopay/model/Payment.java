eff57b174bd5ca18b17dfac1e01ed73da6bdcb11
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-06-29 11:56:18 -0300

    Create domain payment

---
+++ src/main/java/com/hackaton/marjopay/model/Payment.java
@@ -0,0 +1,42 @@
+package com.hackaton.marjopay.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "payment")
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
+	private Long id;
+	
+	private BigDecimal value;
+	
+	@Column(name = "date_transaction")
+	@DateTimeFormat(pattern = "dd/MM/yyyy:HH:mm")
+	private LocalDateTime dateTransaction;
+}
