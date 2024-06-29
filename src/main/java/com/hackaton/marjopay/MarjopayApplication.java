73d4bca41b726c5788f0162355552f59de13a040
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-06-29 11:43:38 -0300

    Project Created

---
+++ src/main/java/com/hackaton/marjopay/MarjopayApplication.java
@@ -0,0 +1,13 @@
+package com.hackaton.marjopay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarjopayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarjopayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackaton/marjopay/MarjopayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackaton.marjopay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarjopayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
