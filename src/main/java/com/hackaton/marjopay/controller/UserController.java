0b40ad864116328a3e8424030d4f002ffdaee4ee
Jussara Granja <jussaragranja19@gmail.com>
2024-06-29 13:36:43 -0300

    add list users

--- src/main/java/com/hackaton/marjopay/controller/UserController.java
+++ src/main/java/com/hackaton/marjopay/controller/UserController.java
@@ -1,12 +1,27 @@
 package com.hackaton.marjopay.controller;
 
+import com.hackaton.marjopay.model.User;
+import com.hackaton.marjopay.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api")
 @CrossOrigin(origins = "*")
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/list-user")
+    public List<User> listUsers(){
+        return userRepository.findAll();
+    }
+
 
 }
