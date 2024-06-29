c490bdcff35ef049312ee0564118b2f285049277
Jussara Granja <jussaragranja19@gmail.com>
2024-06-29 13:23:50 -0300

    add controller and repository

---
+++ src/main/java/com/hackaton/marjopay/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.hackaton.marjopay.repository;
+
+import com.hackaton.marjopay.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User deleteById(long id);
+
+}
